3775ca7a39b3043123459b28a03d025eff8027a8
hwellmann <hwellmann@7eccfe4a-4f19-0410-86c7-be9599f3b344>
2007-06-09 15:26:39 +0000

    added freemarker lib, start using it for generating package.html

--- branches/freemarker/src/datascript/emit/html/PackageEmitter.java
+++ branches/freemarker/src/datascript/emit/html/PackageEmitter.java
@@ -1,38 +1,56 @@
 package datascript.emit.html;
 
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
 import antlr.collections.AST;
-import datascript.jet.html.PackageBegin;
-import datascript.jet.html.PackageEnd;
-import datascript.jet.html.PackageItem;
+import datascript.ast.DataScriptException;
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
 
 
 public class PackageEmitter extends DefaultHTMLEmitter
 {
-    private PackageBegin beginTmpl = new PackageBegin();
-    private PackageItem itemTmpl = new PackageItem();
-    private PackageEnd endTmpl = new PackageEnd();
+	protected Configuration cfg = new Configuration();
+	private List<String> packages = new ArrayList<String>();
 
 
     @Override
     public void beginRoot(AST rootNode)
     {
-        //setPackageName(rootNode.getFirstChild().getFirstChild());
-
-        openOutputFile(directory, "packages" + HTML_EXT);
-        out.print(beginTmpl.generate(this));
+    	cfg.setClassForTemplateLoading(getClass(), "../../..");
+    	cfg.setObjectWrapper(new DefaultObjectWrapper());
     }
 
     @Override
     public void endRoot()
     {
-        out.print(endTmpl.generate(this));
-        out.close();
+    	try
+    	{
+    		Template tpl = cfg.getTemplate("html/package.html.ftl");
+    		openOutputFile(directory, "packages" + HTML_EXT);
+    		Writer writer = new PrintWriter(out);
+    		tpl.process(this, writer);
+    		writer.close();
+    	}
+    	catch (Exception exc)
+    	{
+    		throw new DataScriptException(exc);
+    	}
     }
 
     @Override
     public void endPackage(AST p)
     {
-        out.print(itemTmpl.generate(this));
+        packages.add(getPackageName());
+    }
+    
+    public List<String> getPackages()
+    {
+    	return packages;
     }
 
 }
