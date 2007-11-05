2611c8b11b2c967bf07ec8f0d926ac12cd1e07a6
hwellmann <hwellmann@7eccfe4a-4f19-0410-86c7-be9599f3b344>
2007-11-05 00:55:39 +0000

    upgraded editor plug-in to Eclipse 3.3

--- trunk/net.sourceforge.dstools.eclipse.editor/src/datascript/eclipse/editor/DataScriptDocumentProvider.java
+++ trunk/net.sourceforge.dstools.eclipse.editor/src/datascript/eclipse/editor/DataScriptDocumentProvider.java
@@ -38,13 +38,13 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentPartitioner;
 import org.eclipse.jface.text.rules.FastPartitioner;
-import org.eclipse.ui.editors.text.FileDocumentProvider;
+import org.eclipse.ui.editors.text.TextFileDocumentProvider;
 
-public class DataScriptDocumentProvider extends FileDocumentProvider
+public class DataScriptDocumentProvider extends TextFileDocumentProvider
 {
     protected IDocument createDocument(Object element) throws CoreException
     {
-        IDocument document = super.createDocument(element);
+        IDocument document = getDocument(element);
         if (document != null)
         {
             IDocumentPartitioner partitioner = new FastPartitioner(
