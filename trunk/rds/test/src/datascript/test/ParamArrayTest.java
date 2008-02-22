7e17a555bf8b42b5798ccfa761df849111bce6b5
HWellmann <HWellmann@7eccfe4a-4f19-0410-86c7-be9599f3b344>
2008-02-22 13:05:31 +0000

    DataScriptIO.read() now accepts additional arguments for constructor

--- trunk/rds/test/src/datascript/test/ParamArrayTest.java
+++ trunk/rds/test/src/datascript/test/ParamArrayTest.java
@@ -9,23 +9,29 @@ import java.io.IOException;
 import javax.imageio.stream.FileImageOutputStream;
 
 import junit.framework.TestCase;
-import bits.arrays.ParamArray;
 import bits.ParamBlock;
+import bits.arrays.ParamArray;
+import datascript.runtime.array.ByteArray;
 import datascript.runtime.array.UnsignedShortArray;
+import datascript.runtime.io.DataScriptIO;
 
 /**
  * @author HWellmann
- *
+ * 
  */
 public class ParamArrayTest extends TestCase
 {
     private FileImageOutputStream os;
+
     private String wFileName = "ParamArryWriteTest.data";
+
     private String fileName = "paramarraytest.bin";
+
     private File file = new File(fileName);
 
     /**
      * Constructor for BitStreamReaderTest.
+     * 
      * @param name
      */
     public ParamArrayTest(String name)
@@ -50,7 +56,8 @@ public class ParamArrayTest extends TestCase
         file.delete();
     }
 
-    private void checkArray(ParamArray array, int sizeof, long numItems, byte startValue, short[] size)
+    private void checkArray(ParamArray array, int sizeof, long numItems,
+            byte startValue, short[] size)
     {
         assertEquals(numItems, array.getNumItems());
         UnsignedShortArray sizes = array.getSize();
@@ -60,18 +67,19 @@ public class ParamArrayTest extends TestCase
         }
         for (int i = 0; i < numItems; i++)
         {
-        	ParamBlock block = array.getBlock().elementAt(i);
-        	for (int j = 0; j < size[i]; j++)
-        	{
-        		assertEquals(startValue+j, block.getData().elementAt(j));
-        	}
+            ParamBlock block = array.getBlock().elementAt(i);
+            for (int j = 0; j < size[i]; j++)
+            {
+                assertEquals(startValue + j, block.getData().elementAt(j));
+            }
         }
         assertEquals(sizeof, array.sizeof());
     }
-    
-    private int writeArray(int numItems, byte startValue, short[] size) throws IOException
+
+    private int writeArray(int numItems, byte startValue, short[] size)
+        throws IOException
     {
-    	file.delete();
+        file.delete();
         os = new FileImageOutputStream(file);
 
         os.writeInt(numItems);
@@ -79,35 +87,48 @@ public class ParamArrayTest extends TestCase
         {
             os.writeShort(size[i]);
         }
-        
+
         for (int i = 0; i < numItems; i++)
         {
-        	for (int j = 0; j < size[i]; j++)
-        	{
-        		os.writeByte(startValue+j);
-        	}
+            for (int j = 0; j < size[i]; j++)
+            {
+                os.writeByte(startValue + j);
+            }
         }
-        int sizeof = (int)os.getStreamPosition();
+        int sizeof = (int) os.getStreamPosition();
         os.close();
 
         return sizeof;
     }
-    
+
     /*
      * Test method for 'datascript.library.BitStreamReader.readByte()'
      */
-    public  void testParamArray1() throws Exception
+    public void testParamArray1() throws Exception
     {
-        short[] sizes = new short[] {1, 3, 2};
-        
-    	int size = writeArray((short) 3, (byte) 30, sizes);
+        short[] sizes = new short[] { 1, 3, 2 };
+
+        int size = writeArray((short) 3, (byte) 30, sizes);
         ParamArray array = new ParamArray(fileName);
         checkArray(array, size, (short) 3, (byte) 30, sizes);
-        
+
         array.write(wFileName);
 
         ParamArray array2 = new ParamArray(wFileName);
         checkArray(array2, size, (short) 3, (byte) 30, sizes);
         assertTrue(array.equals(array2));
+    }
+    
+    public void testVirtualConstructor()
+    {
+        int size = 3;
+        ParamBlock block = new ParamBlock();
+        block.setSize(size);
+        byte[] bytes = { 18, 28, 38 };
+        ByteArray byteArray = new ByteArray(bytes, 0, 3);
+        block.setData(byteArray);
+        byte[] blob = DataScriptIO.write(block);
+        ParamBlock block2 = DataScriptIO.read(ParamBlock.class, blob, size);
+        assertEquals(block, block2);
     }
 }
