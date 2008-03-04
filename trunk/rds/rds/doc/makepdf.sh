e225131b0768caed3cc8fc729cba7f8e44b4be22
HWellmann <HWellmann@7eccfe4a-4f19-0410-86c7-be9599f3b344>
2008-03-04 13:45:18 +0000

    reverting from r459 to r458

--- trunk/rds/rds/doc/makepdf.sh
+++
@@ -1,8 +0,0 @@
-xsltproc  --stringparam body.start.indent 0in --stringparam title.margin.left 0in --stringparam fop.extensions 1 --stringparam section.autolabel 1 --stringparam paper.type A4 /usr/share/xml/docbook/stylesheet/nwalsh/1.69.0/fo/docbook.xsl DataScriptLanguageOverview.xml > DataScriptLanguageOverview.fo
-
-fop DataScriptLanguageOverview.fo DataScriptLanguageOverview.pdf
-
-xsltproc  --stringparam body.start.indent 0in --stringparam title.margin.left 0in --stringparam fop.extensions 1 --stringparam section.autolabel 1 --stringparam paper.type A4 /usr/share/xml/docbook/stylesheet/nwalsh/1.69.0/fo/docbook.xsl InstallationGuide.xml > InstallationGuide.fo
-
-fop InstallationGuide.fo InstallationGuide.pdf
-
