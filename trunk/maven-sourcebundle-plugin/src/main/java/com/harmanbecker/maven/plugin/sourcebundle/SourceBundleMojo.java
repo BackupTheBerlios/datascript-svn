665889273966466437e8edde9c38f22ce8de6e56
hwellmann <hwellmann@7eccfe4a-4f19-0410-86c7-be9599f3b344>
2008-11-09 14:21:14 +0000

    - fixed the version number strings and tested that Eclipse indeed recognizes the generated source bundles
    - added Apache License header

--- trunk/maven-sourcebundle-plugin/src/main/java/com/harmanbecker/maven/plugin/sourcebundle/SourceBundleMojo.java
+++ trunk/maven-sourcebundle-plugin/src/main/java/com/harmanbecker/maven/plugin/sourcebundle/SourceBundleMojo.java
@@ -1,20 +1,23 @@
-package com.harmanbecker.maven.plugin.sourcebundle;
-
 /*
- * Copyright 2001-2005 The Apache Software Foundation.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
+package com.harmanbecker.maven.plugin.sourcebundle;
 
 import java.io.File;
 import java.util.List;
@@ -28,7 +31,14 @@ import org.apache.maven.project.MavenProjectHelper;
 import org.codehaus.plexus.archiver.jar.JarArchiver;
 
 /**
- * Goal which touches a timestamp file.
+ * Creates a source bundle for Eclipse 3.4 from the sources of the current
+ * project. 
+ * <p>
+ * Eclipse will only recognize the bundle as a source bundle if it is
+ * renamed to bundleSymbolicName_major.minor.micro[.qualifier].jar, so your
+ * Maven assembly should take care of this.
+ * <p>
+ * Implementation based on org.apache.maven.plugin.jar.AbstractJarMojo.
  *
  * @goal sourcebundle
  * 
@@ -37,7 +47,8 @@ import org.codehaus.plexus.archiver.jar.JarArchiver;
 public class SourceBundleMojo
     extends AbstractMojo
 {
-    private static final String[] DEFAULT_EXCLUDES = new String[] { "**/package.html", "**/CVS/**", "**/.svn/**" };
+    private static final String[] DEFAULT_EXCLUDES = 
+    	new String[] { "**/package.html", "**/CVS/**", "**/.svn/**" };
 
     private static final String[] DEFAULT_INCLUDES = new String[] { "**/**" };
 
@@ -103,7 +114,6 @@ public class SourceBundleMojo
      * @parameter expression="${project.build.outputDirectory}/META-INF/MANIFEST.MF"
      * @required
      * @readonly
-     * @since 2.2
      */
     private File defaultManifestFile;
 
@@ -129,6 +139,7 @@ public class SourceBundleMojo
     private boolean forceCreation;
 
     /**
+     * The root directories for sources to be included.
      * @parameter expression="${project.compileSourceRoots}"
      * @required
      */
@@ -136,10 +147,21 @@ public class SourceBundleMojo
 
 
     /**
-     * @parameter expression="${bundleName}" default-value="${project.artifactId}.source"
+     * The symbolic name of the binary OSGi bundle related to this source
+     * bundle.
+     *  
+     * @parameter expression="${bundleName}" default-value="${project.artifactId}"
      * @required
      */
     private String bundleName;
+    
+    /**
+     * The bundle symbolic name of the source bundle itself.
+     * 
+     * @parameter expression="${sourceBundleName}" default-value="${project.artifactId}.source"
+     * @required
+     */
+    private String sourceBundleName;
     
     protected final MavenProject getProject()
     {
@@ -206,15 +228,20 @@ public class SourceBundleMojo
 
         try
         {
+        	// Add the source directories to the archiver
             for (String sourceRootName : compileSourceRoots)
             {
                 File sourceDirectory = new File(sourceRootName);
                 archiver.getArchiver().addDirectory( sourceDirectory, getIncludes(), getExcludes() );                
             }
+            String version = getVersion();
+            String sourceBundleHeader = String.format("%s;version=\"%s\"", bundleName, version);
             
-            String sourceBundleHeader = String.format("%s;version=\"%s\"", bundleName, project.getVersion());
+            // Add the OSGi and Eclipse manifest headers
+            archive.addManifestEntry("Bundle-ManifestVersion", "2");
+            archive.addManifestEntry("Bundle-SymbolicName", sourceBundleName);
+            archive.addManifestEntry("Bundle-Version", version);
             archive.addManifestEntry("Eclipse-SourceBundle", sourceBundleHeader);
-            
             File existingManifest = getDefaultManifestFile();
 
             if ( useDefaultManifestFile && existingManifest.exists() && archive.getManifestFile() == null )
@@ -271,5 +298,81 @@ public class SourceBundleMojo
             return excludes;
         }
         return DEFAULT_EXCLUDES;
+    }
+    
+    /**
+     * Converts the Maven artifact version to a version in OSGi format
+     * major.minor.micro.qualifier, such that major, minor and micro are
+     * non-negative decimals. 
+     * 
+     * TODO: This implementation is not fool-proof and should probably be
+     * shared with similar code in the maven-bundle-plugin, or the bnd tool,
+     * or Eclipse PDE.
+     * 
+     * @return OSGi version string
+     */
+    private String getVersion()
+    {
+    	String[] parts = project.getVersion().split("\\.");
+    	String major = "0";
+    	String minor = "0";
+    	String micro = "0";
+    	String qualifier = null;
+    	if (isNumber(parts[0]))
+    	{
+    		major = parts[0];
+    	}
+    	else
+    	{
+    		throw new IllegalArgumentException(project.getVersion());
+    	}
+    	if (parts.length > 1)
+    	{
+    		if (isNumber(parts[1]))
+    		{
+    			minor = parts[1];
+    		}
+    		else
+    		{
+    			qualifier = parts[1];
+    		}
+    	}
+    	if (parts.length > 2)
+    	{
+    		if (isNumber(parts[2]))
+    		{
+    			minor = parts[2];
+    		}
+    		else
+    		{
+    			qualifier = parts[2];
+    		}
+    	}
+    	if (parts.length > 3)
+    	{
+    		qualifier = parts[3];
+    	}
+    	
+    	String version = String.format("%s.%s.%s", major, minor, micro);
+    	if (qualifier != null && qualifier.length() > 0)
+    	{
+    		version += "." + qualifier;
+    	}
+    	return version;
+    }
+    
+    /**
+     * Checks if the String is composed of digits only.
+     * @param s
+     * @return
+     */
+    private boolean isNumber(String s)
+    {
+    	for (int i = 0; i < s.length(); i++)
+    	{
+    		if (! Character.isDigit(s.charAt(i)))
+    			return false;	
+    	}
+    	return true;
     }
 }
